9c318f67b6df1c76bda8ecc99436a717c467aea6
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-12 23:20:02 +0530

    form submission improve

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,34 +1,31 @@
-import React,{useState} from 'react'
-import Extraction from './Extraction'
-import Logo from '../Images/logo.png'
-import { UserEnteredInput } from '../Context/SearchContext'
-import LoadingBar from 'react-top-loading-bar'
+import React, { useState } from 'react';
+import Logo from '../Images/logo.png';
+import { UserEnteredInput } from '../Context/SearchContext';
 
-export default function Search() {
-    const {SearchInput , setSearchInput} = UserEnteredInput()
-    // const [progressbar , setprogressbar]  = useState(0)
-    function inputchange(e){
-        setSearchInput(e)
-    }
-    function enterkey(e){
-      e.preventDefault()
-    }
+const Search = () => {
+  const [UserInput, setUserInput] = useState('')
+  const {setSearchInput } = UserEnteredInput();
+
+  const handleEnterKey = (e) => {
+    e.preventDefault();
+    setSearchInput(UserInput)
+  };
+
   return (
     <div>
-      {/* <LoadingBar
-      color='#000000'
-      height={3}
-      progress={progressbar}
-      onLoaderFinished={()=>{setprogressbar(0)}}
-      
-      /> */}
-        <div className='universal search-box'>
-      <form className='universal' action="">
-        <img src={Logo} alt="" />
-        <input type="text" onSubmit={enterkey} value={SearchInput} onChange={(e)=>inputchange(e.target.value)} placeholder='Search Image' name="" id="" />
-      </form>
-        </div>
-      {/* <Extraction settingtopbar= {setprogressbar} inputwords={SearchInput}/> */}
+      <div className='universal search-box'>
+        <form className='universal' onSubmit={handleEnterKey}>
+          <img src={Logo} alt="" />
+          <input
+            type="text"
+            value={UserInput}
+            onChange={(e)=>setUserInput(e.target.value)}
+            placeholder='Search Image'
+          />
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Search;
