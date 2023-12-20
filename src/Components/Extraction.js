16d8f36991601c34269827c164bd69b9ae160ba6
jyotirmay <jyotirmay2000gupta@gmail.com>
2023-12-20 22:11:23 +0530

    removed additional features

--- src/Components/Extraction.js
+++ src/Components/Extraction.js
@@ -5,51 +5,29 @@ import { UserEnteredInput } from "../Context/SearchContext";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Extraction() {
-  // let { inputwords, settingtopbar } = props;
   const {SearchInput}= UserEnteredInput();
   const [page, setpage] = useState(1);
   const [newdata, setnewdata] = useState([]);
-  const [inputvalue, newinputvalue] = useState("");
-  const [loading, setloading] = useState(false);
   const api_key = process.env.React_App_wallpaper_app;
 
-  useEffect(() => {
-    newinputvalue(SearchInput);
-  }, [SearchInput]);
-
-  console.log("am i able to get searching = ", SearchInput)
   useEffect(() => {
     fetchdata();
     async function fetchdata() {
       let url;
-      // settingtopbar(20);
       try {
-        if (inputvalue === "") {
-          setloading(true);
-          // settingtopbar(40);
+        if (SearchInput === "") {
           url = `https://api.unsplash.com/photos/?page=${page}&client_id=${api_key}`;
           let fetchdata = await fetch(url);
-          // settingtopbar(60);
           let data = await fetchdata.json();
-          // settingtopbar(80);
           setnewdata(data);
-          setloading(false);
-          // settingtopbar(100);
         } else {
-          setloading(true);
-          // settingtopbar(40);
-          url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputvalue}&client_id=${api_key}`;
-          // settingtopbar(60);
+          url = `https://api.unsplash.com/search/photos?page=${page}&query=${SearchInput}&client_id=${api_key}`;
           let fetchdata = await fetch(url);
-          // settingtopbar(80);
           let data = await fetchdata.json();
           setnewdata(data.results);
-          // settingtopbar(100);
-          setloading(false);
         }
       } catch (error) {
         console.log("getting error, not able to fetch data");
-        // settingtopbar(60);
       }
     }
   }, [SearchInput]);
@@ -57,8 +35,7 @@ export default function Extraction() {
   async function fetchmoredata() {
     let url;
     try {
-      if (inputvalue === "") {
-        setloading(true);
+      if (SearchInput === "") {
         url = `https://api.unsplash.com/photos/?page=${
           page + 1
         }&client_id=${api_key}`;
@@ -66,17 +43,14 @@ export default function Extraction() {
         let data = await fetchdata.json();
         setnewdata(newdata.concat(data));
         setpage(page + 1);
-        setloading(false);
       } else {
-        setloading(true);
         url = `https://api.unsplash.com/search/photos?page=${
           page + 1
-        }&query=${inputvalue}&client_id=${api_key}`;
+        }&query=${SearchInput}&client_id=${api_key}`;
         let fetchdata = await fetch(url);
         let data = await fetchdata.json();
         setnewdata(newdata.concat(data.results));
         setpage(page + 1);
-        setloading(false);
       }
     } catch (error) {
       console.log("getting error, not able to fetch data");
@@ -84,7 +58,6 @@ export default function Extraction() {
   }
   return (
     <>
-      {loading && <Spinner />}
       {newdata.length > 0 && 
       <InfiniteScroll
         className="universal big-box-add"
